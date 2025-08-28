48fcd2fe1efdc056788fcf08923c94c16501a09b
Suhawani Shukla <shuklasuhawani5@gmail.com>
2025-08-28 21:19:14 +0530

    basic constructor code for understanding

--- constructor.java
+++ constructor.java
@@ -1,17 +1,19 @@
-package project1;
-
+package oops;
+//code for understanding of constructor
 public class constructor {
     int a;String name;
+    //constructor
     constructor (){
         a = 7;name = "shush";
     }
+    //method
     void show(){
         System.out.print(a+" "+name);
     }
 }
 class b{
     public static void main(String[] args) {
-        constructor ref = new constructor();
-        ref.show();
+        constructor ref = new constructor(); //object creation
+        ref.show(); // print value
     }
 }
