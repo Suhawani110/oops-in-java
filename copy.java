b35b294ecd9ccd69e4904cb0b137dccb25af9b68
Suhawani Shukla <shuklasuhawani5@gmail.com>
2025-08-28 23:17:56 +0530

    copy constructor code

--- copy.java
+++ copy.java
@@ -1,20 +1,22 @@
-package project1;
+package oops;
 
 public class copy {
     int a;String b;
+    //Default / no-arg constructor
     copy(){
         a = 10;b = "learn coding";
         System.out.println(a+" "+b);
     }
+    //copy constructor
     copy(copy ref){
-        a = ref.a;
-        b =ref.b;
+        a = ref.a; //copy value of a fron another object
+        b =ref.b; // copy value of b from another object
         System.out.println(a+" "+b);
     }
 }
-class D{
+class N{
     public static void main(String[] args){
-        copy r = new copy();
-        copy r1 = new copy(r);
+        copy r = new copy(); //calls default constructor
+        copy r1 = new copy(r); // calls copy constructor
     }
 }
