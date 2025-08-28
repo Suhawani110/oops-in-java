9bb7ed39442b4dcbd93deedd5bdd4e6ee2f379d6
Suhawani Shukla <shuklasuhawani5@gmail.com>
2025-08-28 22:16:06 +0530

    parametrised constructor code

--- parameterised.java
+++ parameterised.java
@@ -1,18 +1,20 @@
-package project1;
+package oops;
 
 public class parameterised {
     int x,y;
+    //constructor
     parameterised(int a,int b){
         this.x = a;
         this.y = b;
     }
+    //method
     void show(){
             System.out.println(x+" "+y);
         }
     }
 class C{
     public static void main(String[] args){
-        parameterised r = new parameterised(100,200);
-        r.show();
+        parameterised r = new parameterised(100,200); //object creation
+        r.show();// method call
     }
 }
