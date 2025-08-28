2268318b4a3d5ce04d9c48c890d16f630e5834b7
Suhawani Shukla <shuklasuhawani5@gmail.com>
2025-08-28 22:05:11 +0530

    default construction code

--- defaultCons.java
+++ defaultCons.java
@@ -1,19 +1,21 @@
-package project1;
-
+package oops;
+//code for understanding default constructor
 public class defaultCons {
     int a;String b;boolean c;
+    //constructor
     defaultCons(){
         a =10;
         b="hello";
         c=true;
     }
+    //method
     void show(){
         System.out.println(a+" "+b+" "+c);
     }
 }
-class B{
+class b{
     public static void main(String[] args){
-        defaultCons r = new defaultCons();
-        r.show();
+        defaultCons r = new defaultCons();//object creation
+        r.show();//method call
     }
 }
